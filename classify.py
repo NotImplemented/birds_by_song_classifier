670b74c7d2ec4c64c2a5ff5f652bc08123ce575d
Mikhail_Kaspiarovich <kasperovichm@gmail.com>
2017-02-01 13:30:42 +0300

    CNN for classification

--- classify.py
+++ classify.py
@@ -22,6 +22,8 @@ def bias_variable(shape):
 
 def prepare_dataset():
 
+    pixel_type = numpy.dtype(numpy.uint8)
+
     labels = []
 
     file_labels = open(os.path.join('mlsp_contest_dataset', 'essential_data', 'rec_labels_test_hidden.txt'))
@@ -41,12 +43,17 @@ def prepare_dataset():
             continue
 
         classes = line.split(',')
-        label = numpy.zeros((1, output_classes))
+        label = numpy.ndarray((1, output_classes))
 
         for c in range(1, len(classes)):
             label[(0,classes[c])] = 1.0
 
         image = matplotlib.image.imread(file_path)
+        image.reshape((1, input_size_height * input_size_width))
+        pixel_type = numpy.dtype(numpy.uint8)
+
+        image = (image - (numpy.iinfo(pixel_type).max - numpy.iinfo(pixel_type).min) / 2.0) / (
+        numpy.iinfo(pixel_type).max - numpy.iinfo(pixel_type).min)
 
         images.append(image)
         labels.append(label)
@@ -69,7 +76,6 @@ x_image = tensorflow.reshape(x_place, [-1, input_size_height, input_size_width, 
 W_conv_1st = weight_variable([5, 5, 1, 32])
 b_conv_1st = bias_variable([32])
 
-
 h_conv_1st = tensorflow.nn.relu(conv2d(x_image, W_conv_1st) + b_conv_1st)
 h_pool_1st = max_pool_2x2(h_conv_1st)
 
@@ -81,15 +87,18 @@ b_conv_2nd = bias_variable([64])
 h_conv_2nd = tensorflow.nn.relu(conv2d(h_pool_1st, W_conv_2nd) + b_conv_2nd)
 h_pool_2nd = max_pool_2x2(h_conv_2nd)
 
+
+
+
 # Fully connected 1st layer
-W_fc_1st = weight_variable([7 * 7 * 64, 1024])
-b_fc_1st = bias_variable([1024])
+W_fc_1st = weight_variable([(input_size_height / 2) * (input_size_width / 2) * 64, 64])
+b_fc_1st = bias_variable([64])
 
 h_pool_2nd_flat = tensorflow.reshape(h_pool_2nd, [-1, 7 * 7 * 64])
 h_fc_1st = tensorflow.nn.relu(tensorflow.matmul(h_pool_2nd_flat, W_fc_1st) + b_fc_1st)
 
 # Fully connected 2nd layer
-W_fc_2nd = weight_variable([1024, output_classes])
+W_fc_2nd = weight_variable([64, output_classes])
 b_fc_2nd = bias_variable([output_classes])
 
 y_conv = tensorflow.matmul(h_fc_1st, W_fc_2nd) + b_fc_2nd
@@ -104,27 +113,27 @@ session = tensorflow.InteractiveSession()
 session.run(tensorflow.global_variables_initializer())
 
 (images, labels) = prepare_dataset()
-batch_size = 1
+batch_size = 32
 index = 0
-
-from tensorflow.examples.tutorials.mnist import input_data
-mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
-t = mnist.train.next_batch(50)
 
 for i in range(learning_steps):
 
-    batch = numpy.ndarray((batch_size, input_size_height, input_size_width))
+    batch = numpy.ndarray((batch_size, input_size_height*input_size_width))
     label = numpy.ndarray((batch_size, output_classes))
 
     for j in range(batch_size):
 
-        batch[j] = images[index % len(images)]
-        label[j] = labels[index % len(images)]
+        batch[j,:] = images[index % len(images)].reshape((input_size_height*input_size_width))
+        label[j,:] = labels[index % len(images)]
         ++index
 
     if i % 64 == 0:
+
+        #batch = batch.reshape((1, batch_size * input_size_height * input_size_width))
+        #label = label.reshape((batch_size*output_classes))
+
         train_accuracy = accuracy.eval(feed_dict={x_place: batch, y_place: label})
         print("Step %d: training accuracy = %g" % (i, train_accuracy))
 
-    train_step.run(feed_dict={x_place: batch[0], y_place: batch[1]})
+    train_step.run(feed_dict={x_place: batch, y_place: label})
 
